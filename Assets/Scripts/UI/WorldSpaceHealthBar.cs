895a65356e55e43a4facb6bf9960c8f0483c9cf0
SantiR <santiagoromero121@gmail.com>
2026-06-17 16:56:12 -0300

    re assing health bar in player

--- Assets/Scripts/UI/WorldSpaceHealthBar.cs
+++ Assets/Scripts/UI/WorldSpaceHealthBar.cs
@@ -26,12 +26,7 @@ public sealed class WorldSpaceHealthBar : MonoBehaviour
             return;
         }
 
-        // Anchor + pivot izquierda para que el ancho crezca/encoge de derecha a izquierda
-       // fillRect.anchorMin = new Vector2(0f, 0f);
-        //fillRect.anchorMax = new Vector2(0f, 1f);
-        //fillRect.pivot     = new Vector2(0f, 0.5f);
-        //fillRect.offsetMin = Vector2.zero;
-        //fillRect.offsetMax = Vector2.zero;
+
 
         _maxWidth = ghostRect.rect.width;
     }
